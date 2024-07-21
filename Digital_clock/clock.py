9d176188f0c457517e7ac24b3f151613543754a4
keyurbhatiya <keyurbhatiya3004@gamil.com>
2024-07-21 14:14:13 +0530

    keyur

---
+++ Digital_clock/clock.py
@@ -0,0 +1,1 @@
+import tkinter as tkfrom time import strftimeroot = tk.Tk()root.title("Digital Clock")def time():    string = strftime('%I:%M:%S %p \n %m/%d/%Y')    label.config(text=string)    label.after(1000, time)label = tk.Label(root, font=('calibri', 50, 'bold'), background='black', foreground='white')label.pack(anchor='center')time()root.mainloop()
