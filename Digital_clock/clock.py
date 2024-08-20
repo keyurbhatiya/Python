1776b17ac30066fcbd3b8b4cac15bedab5ec06ed
Keyur Bhatiya <115718562+keyurbhatiya@users.noreply.github.com>
2024-08-20 15:04:20 +0530

    Update clock.py

--- Digital_clock/clock.py
+++ Digital_clock/clock.py
@@ -1,1 +1,17 @@
-import tkinter as tkfrom time import strftimeroot = tk.Tk()root.title("Digital Clock")def time():    string = strftime('%I:%M:%S %p \n %m/%d/%Y')    label.config(text=string)    label.after(1000, time)label = tk.Label(root, font=('calibri', 50, 'bold'), background='black', foreground='white')label.pack(anchor='center')time()root.mainloop()
+import tkinter as tk
+from time import strftime
+
+root = tk.Tk()
+root.title("Digital Clock")
+
+def time():
+    string = strftime('%I:%M:%S %p \n %m/%d/%Y')
+    label.config(text=string)
+    label.after(1000, time)
+
+label = tk.Label(root, font=('calibri', 50, 'bold'), background='black', foreground='white')
+label.pack(anchor='center')
+
+time()
+
+root.mainloop()
